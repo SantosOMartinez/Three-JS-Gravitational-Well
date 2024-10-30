00b3c8549c70c66d3c438daa250af918c40423d6
Santos O. Martínez <martinezsantos3@gmail.com>
2024-10-30 17:35:34 -0400

    Added ability to update geometry through gui

--- src/script.js
+++ src/script.js
@@ -5,11 +5,14 @@ import GUI from "lil-gui";
 import vertexShader from "./shaders/plot.vert";
 import fragmentShader from "./shaders/plot.frag";
 import createWeb from "./web-geometry";
+import Stats from "three/examples/jsm/libs/stats.module.js";
 /**
  * Base
  */
 // Debug
 const gui = new GUI();
+const stats = new Stats();
+document.body.appendChild(stats.dom);
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -22,11 +25,21 @@ const scene = new THREE.Scene();
  */
 const textureLoader = new THREE.TextureLoader();
 
+const config = {
+  play: true,
+  radius: 10,
+  layers: 20,
+  segments: 20,
+};
+
+const createGeometry = () =>
+  createWeb(config.radius, config.layers, config.segments);
+
 /**
  * Test mesh
  */
 // Geometry
-const geometry = createWeb(10);
+const geometry = createGeometry();
 
 // Material
 const material = new THREE.ShaderMaterial({
@@ -48,8 +61,9 @@ const mesh = new THREE.LineSegments(geometry, material);
 mesh.rotation.x = (3 * Math.PI) / 2;
 scene.add(mesh);
 
-const config = {
-  play: true,
+const updateGeometry = () => {
+  mesh.geometry.dispose();
+  mesh.geometry = createGeometry();
 };
 
 gui
@@ -62,13 +76,37 @@ gui
 gui
   .add(material.uniforms.u_inner_radius, "value")
   .min(0)
-  .max(10)
+  .max(100)
   .step(0.01)
   .name("Inner Radius");
 
 gui.add(config, "play").name("Play");
 
 gui.addColor(material.uniforms.u_color, "value").name("color");
+
+const geometryGui = gui.addFolder("Geometry");
+
+geometryGui
+  .add(config, "radius")
+  .min(0)
+  .max(100)
+  .step(1)
+  .name("Radius")
+  .onChange(updateGeometry);
+geometryGui
+  .add(config, "layers")
+  .min(0)
+  .max(1_000)
+  .step(1)
+  .name("Layers")
+  .onChange(updateGeometry);
+geometryGui
+  .add(config, "segments")
+  .min(0)
+  .max(1_000)
+  .step(1)
+  .name("Segments")
+  .onChange(updateGeometry);
 
 const translations = gui.addFolder("Translation");
 translations.add(mesh.position, "x").min(-10).max(10).step(0.01).name("X");
@@ -125,7 +163,7 @@ const camera = new THREE.PerspectiveCamera(
   75,
   sizes.width / sizes.height,
   0.1,
-  100
+  2_000
 );
 camera.position.set(0, 10, 30);
 scene.add(camera);
@@ -137,9 +175,7 @@ controls.enableDamping = true;
 /**
  * Renderer
  */
-const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-});
+const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
@@ -156,9 +192,9 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime() / 5;
 
   if (config.play) {
-    gui.controllers[0].setValue(snap(Math.abs(Math.sin(elapsedTime) * 10)));
+    gui.controllers[0].setValue(Math.abs(Math.sin(elapsedTime) * 10));
     gui.controllers[1].setValue(
-      snap(Math.abs(Math.sin(elapsedTime + 2) * 9) + 1)
+      Math.abs(Math.sin(elapsedTime + 2) * config.radius) + config.radius * 0.5
     );
   }
 
@@ -167,6 +203,7 @@ const tick = () => {
 
   // Render
   renderer.render(scene, camera);
+  stats.update();
 
   // Call tick again on the next frame
   window.requestAnimationFrame(tick);
